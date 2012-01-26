e7e4d70b77c4668fb24088f458fa434aafec216a
Andrew Pepler <info@vertism.com>
2012-01-26 17:00:10 +0000

    added loading of local env variables

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+#Load heroku vars
+heroku_env = Rails.root.join('config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
