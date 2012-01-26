b8342822472ab2a63894dbad15a84e798d5789c5
Andrew Pepler <info@vertism.com>
2012-01-26 16:28:25 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  
+  def main   
+    up_token = ENV['UP_TOKEN']
+    up = Jawbone::Session.new_from_token up_token
+    yesterday = Date.today - 1
+    summary = Jawbone::DailySummary.new(up.daily_summary yesterday)
+    @summary = "Yesterday I " + summary.data["sleep_summary"]["sleeps"].first["title"].downcase! + " and walked for " + summary.kilometers.to_s + " km"
+    
+    last_api = ENV['LASTFM_API']
+    last_secret = ENV['LASTFM_SECRET']
+    lastfm = Lastfm.new(last_api, last_secret)
+    tracks = lastfm.user.get_recent_tracks "pepler"
+    @lastTrack = "The last song I listened to was " + tracks.first["name"] + " by " + tracks.first["artist"]["content"]
+  end
+end
