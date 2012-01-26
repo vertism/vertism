8d37637003eb838b28ca0e317a00ea77f575172a
Andrew Pepler <info@vertism.com>
2012-01-26 17:26:50 +0000

    modified bars

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
 #Load heroku vars
 heroku_env = Rails.root.join('config', 'heroku_env.rb')
 load(heroku_env) if File.exists?(heroku_env)
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Vertism::Application.initialize!
